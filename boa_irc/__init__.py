12c45744264c2b37f050f93fd79943ad5398dd23
Marvin Scholz <epirat07@gmail.com>
2016-06-19 02:39:50 +0200

    Change imports in __init__ file

--- boa_irc/__init__.py
+++ boa_irc/__init__.py
@@ -1,3 +1,3 @@
-from boa_irc_base import BoaIRCBase
-from boa_irc import BoaIRC
-from boa_irc_base import MetaCommands
+from .boa_irc_base import BoaIRCBase
+from .boa_irc import BoaIRC
+from .boa_irc_base import MetaCommands
